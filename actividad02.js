34e36e71f5246015413382aff8d00b1d9ba502de
Eleazar01 <138905004+Eleazar01@users.noreply.github.com>
2023-07-17 17:30:14 +0100

    Add files via upload

---
+++ actividad02.js
@@ -0,0 +1,21 @@
+console.log("Introduce el primer número");
+
+var x = parseInt(prompt("Introduce el primer número"));
+
+console.log("Introduce el segundo número");
+
+var y = parseInt(prompt("Introduce el segundo número"));
+
+var resultado = 0;
+
+resultado = x + y;
+console.log(resultado);
+
+resultado = x - y;
+console.log(resultado);
+
+resultado = x * y;
+console.log(resultado);
+
+resultado = x / y;
+console.log(resultado); 
