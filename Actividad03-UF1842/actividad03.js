94d76305a917bc12e1a8dc7ab42f256a0dafdc35
Eleazar01 <proyectos.eleazargon8@msn.com>
2023-07-17 18:02:49 +0100

    actividad03

---
+++ Actividad03-UF1842/actividad03.js
@@ -0,0 +1,17 @@
+console.log("Introduce el primer número");
+var x = parseInt(prompt("Introduce el primer número"));
+console.log("Introduce el segundo número");
+var y = parseInt(prompt("Introduce el segundo número"));
+if (x>=y){
+    console.log("Si");
+}else if(x==y){
+    console.log("Son iguales");
+}else{
+    console.log(x +" es el mayor de los dos");
+}
+var z= (x===y);
+if (z = false){
+    console.log(y +" es el mayor");
+}else{
+    console.log(y + " es el mayor de los dos");
+}
