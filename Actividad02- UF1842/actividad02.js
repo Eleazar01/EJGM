de689f91a20bd1bc8605f48486586ca09f0abbea
Eleazar01 <proyectos.eleazargon8@msn.com>
2023-07-18 15:26:28 +0100

    clase

--- Actividad02- UF1842/actividad02.js
+++ Actividad02- UF1842/actividad02.js
@@ -1,10 +1,10 @@
 console.log("Introduce el primer número");
 
-var x = parseInt(prompt("Introduce el primer número"));
+var x = parseInt(prompt("Introduce el primer número"));// parseInt solo coge numeros y anula el texto q entre, no admite decimales
 
 console.log("Introduce el segundo número");
 
-var y = parseInt(prompt("Introduce el segundo número"));
+var y = Number(prompt("Introduce el segundo número"));// number, solo admite numeros, si le ponemos texto nos respondera que el dato no es valido
 
 var resultado = 0;
 
