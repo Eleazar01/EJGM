68613d2922a2603b3bca01b658a971ac986a5cb6
Eleazar01 <proyectos.eleazargon8@msn.com>
2023-07-19 16:42:07 +0100

    cambio

--- Examen/Prueba E1 UF1842/Prueba E2 UF1842.js
+++ Examen/Prueba E1 UF1842/Prueba E2 UF1842.js
@@ -1,10 +1,12 @@
 document.write("¿Tienes el titulo de bachiller?"+"<br/>");
 var bachiller = prompt("¿Tienes el titulo de bachiller?");
+document.write(bachiller+"<br/>");
 if (bachiller == "si"){
     document.write("Puedes acceder al grado superior");
 }else{
     document.write("¿Tienes la prueba de acceso superada?"+"<br/>");
     var prueba = prompt("Tienes la prueba de acceso superada");
+    document.write(prueba+"<br/>");
     if(prueba =="si"){
         document.write("Puedes acceder al gado superior");
     }else{
