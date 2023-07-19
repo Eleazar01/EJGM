7fedfc110d6a77b0a6a684661cd7671282e4edb2
Eleazar01 <proyectos.eleazargon8@msn.com>
2023-07-19 19:50:21 +0100

    &&

--- Actividad10- UF1842/Actividad10.js
+++ Actividad10- UF1842/Actividad10.js
@@ -3,19 +3,22 @@ var mes = prompt("Introduce el mes");
 var pfinal= 0;
 document.write("El precio es  "+precio + " €"+"<br/>");
 document.write("El mes es "+mes +"<br/>");
+
 mes = (mes.toLowerCase());
+
 switch(mes) {
-    case "enero" || "febrero":
+    case "enero" && "febrero":
         pfinal = precio*0.80;
         document.write("El precio final es "+pfinal +"<br/>");
-    break;
-    case "julio" || "agosto" || "octubre":
+        break;
+    case "julio" && "agosto" && "octubre":
         pfinal = precio*0.85;
         document.write("El precio final es "+pfinal +"<br/>");
-    break;
-    case "noviembre" || "diciembre":
+        break;
+    case "noviembre" && "diciembre":
         pfinal = precio*0.75;
         document.write("El precio final es "+pfinal +"<br/>");
+        break;
     default:
         pfinal = precio;
         document.write("El precio final es "+pfinal +"<br/>");
