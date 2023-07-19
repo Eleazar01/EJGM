478beb964f80eaece0add1b71784b9f6cd243ff7
Eleazar01 <proyectos.eleazargon8@msn.com>
2023-07-19 17:27:27 +0100

    multiplicador

---
+++ Actividad09- UF1842/actividad09.js
@@ -0,0 +1,127 @@
+// var x =parseInt(prompt("¿Introducir el número de la tabla que quieres ver?"));
+for(var x=0; x<=10; x++)
+switch (x) {
+    case 1:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+    case 2:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+    case 3:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+    case 4:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+    case 5:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+    case 6:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+    case 7:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+    case 8:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+    case 9:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+    case 10:
+        document.write(x+" x 1 = "+(y=x*1+"<br/>"));
+        document.write(x+" x 2 = "+(y=x*2+"<br/>"));
+        document.write(x+" x 3 = "+(y=x*3+"<br/>"));
+        document.write(x+" x 4 = "+(y=x*4+"<br/>"));
+        document.write(x+" x 5 = "+(y=x*5+"<br/>"));
+        document.write(x+" x 6 = "+(y=x*6+"<br/>"));
+        document.write(x+" x 7 = "+(y=x*7+"<br/>"));
+        document.write(x+" x 8 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 9 = "+(y=x*8+"<br/>"));
+        document.write(x+" x 10 = "+(y=x*10+"<br/>"));
+        break;
+
+    default:
+        break;
+}
