5d78df23f5848d3135d41af68bbc9ae02e929817
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2023-07-20 12:00:22 +0100

    Update Actividad10.js

--- Actividad10- UF1842/Actividad10.js
+++ Actividad10- UF1842/Actividad10.js
@@ -11,7 +11,7 @@ switch(mes) {
         pfinal = precio*0.80;
         document.write("El precio final es "+pfinal +"<br/>");
         break;
-    case "julio" && "agosto" && "octubre":
+    case "julio" && "agosto" && "octubre"://quita octubre la actividad ponia solo julio y agosto
         pfinal = precio*0.85;
         document.write("El precio final es "+pfinal +"<br/>");
         break;
@@ -24,3 +24,29 @@ switch(mes) {
         document.write("El precio final es "+pfinal +"<br/>");
         break;
 }
+
+//En enero no me hace el cálculo. Y es porque cuando utilizamos un operador lógico en un case -Booleano- nos cogerá el primer valor o el último dependiendo de si utilizamos OR ó AND. 
+
+//Prueba de esta manera:
+
+/*switch(mes) {
+    case "enero":
+    case "febrero":
+        pfinal = precio*0.80;
+        document.write("El precio final es "+pfinal +"<br/>");
+        break;
+    case "julio":
+    case "agosto":
+        pfinal = precio*0.85;
+        document.write("El precio final es "+pfinal +"<br/>");
+        break;
+    case "noviembre":
+    case "diciembre":
+        pfinal = precio*0.75;
+        document.write("El precio final es "+pfinal +"<br/>");
+        break;
+    default:
+        pfinal = precio;
+        document.write("El precio final es "+pfinal +"<br/>");
+        break;
+}*/
