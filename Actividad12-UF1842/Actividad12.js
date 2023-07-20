063c43160288f0b535c621ff410e01263043007e
Eleazar01 <eleazargon8@msn.com>
2023-07-20 18:44:57 +0100

    1

--- Actividad12-UF1842/Actividad12.js
+++ Actividad12-UF1842/Actividad12.js
@@ -3,11 +3,11 @@ var op2 = prompt("Operador 2");
 document.write("El operador 1 es "+op1+" es "+typeof(op1)+"<br/>");
 document.write("El operador 2 es "+op2+" es "+typeof(op2)+"<br/>");
 var resultado = (op1+op2);
-document.write("El resultado de la suma es "+resultado+" es "+typeof(resultado)+"<br/>");
+document.write("El resultado de la suma es "+resultado+" y es "+typeof(resultado)+"<br/>");
 resultado = (op1-op2);
-document.write("El resultado de la resta es "+resultado+" es "+typeof(resultado)+"<br/>");
+document.write("El resultado de la resta es "+resultado+" y es "+typeof(resultado)+"<br/>");
 resultado = eval((op1+op2));
-document.write("El resultado de la suma con eval es "+resultado+" es "+typeof(resultado)+"<br/>");
+document.write("El resultado de la suma con eval es "+resultado+" y es "+typeof(resultado)+"<br/>");
 
 
 
