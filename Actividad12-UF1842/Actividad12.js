b701e8832bb150e78ed4f5418cbe63bd95c6a557
Eleazar01 <eleazargon8@msn.com>
2023-07-20 18:43:47 +0100

    1

--- Actividad12-UF1842/Actividad12.js
+++ Actividad12-UF1842/Actividad12.js
@@ -1,13 +1,13 @@
-// var op1 = prompt("Operador1");
-// var op2 = prompt("Operador 2");
-// document.write("El operador 1 es "+op1+" es "+typeof(op1)+"<br/>");
-// document.write("El operador 2 es "+op2+" es "+typeof(op2)+"<br/>");
-// var resultado = (op1+op2);
-// document.write("El resultado de la suma es "+resultado+" es "+typeof(resultado)+"<br/>");
-// resultado = (op1-op2);
-// document.write("El resultado de la resta es "+resultado+" es "+typeof(resultado)+"<br/>");
-// resultado = eval((op1+op2));
-// document.write("El resultado de la suma con eval es "+resultado+" es "+typeof(resultado)+"<br/>");
+var op1 = prompt("Operador1");
+var op2 = prompt("Operador 2");
+document.write("El operador 1 es "+op1+" es "+typeof(op1)+"<br/>");
+document.write("El operador 2 es "+op2+" es "+typeof(op2)+"<br/>");
+var resultado = (op1+op2);
+document.write("El resultado de la suma es "+resultado+" es "+typeof(resultado)+"<br/>");
+resultado = (op1-op2);
+document.write("El resultado de la resta es "+resultado+" es "+typeof(resultado)+"<br/>");
+resultado = eval((op1+op2));
+document.write("El resultado de la suma con eval es "+resultado+" es "+typeof(resultado)+"<br/>");
 
 
 
