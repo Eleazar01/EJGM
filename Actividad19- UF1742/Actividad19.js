25fc2f401c9a97891f6528832d3516239699072f
Eleazar01 <eleazargon8@msn.com>
2023-07-25 19:47:19 +0100

    19

--- Actividad19- UF1742/Actividad19.js
+++ Actividad19- UF1742/Actividad19.js
@@ -1,20 +1,19 @@
-
+var alumnos = [];
 function fichaalumno() {
-    let alumnos = [];
     let alumno = [];
-        alumno.push(document.getElementById("codigo").value);
+    alumno.push(document.getElementById("codigo").value);
     
-        alumno.push(document.getElementById("nombre").value);
-    
-        alumno.push(document.getElementById("edad").value);
-    
-        alumno.push(document.getElementById("email").value);
-    
-        alumno.push(document.getElementById("movil").value);
-    
-        alumno.push(document.getElementById("tipos").value);
+    alumno.push(document.getElementById("nombre").value);
         
-        alumnos[].push(alumno);
-    document.getElementById("resultado").innerHTML= alumnos;
-}
+    alumno.push(document.getElementById("edad").value);
+        
+    alumno.push(document.getElementById("email").value);
+        
+    alumno.push(document.getElementById("movil").value);
+        
+    alumno.push(document.getElementById("tipos").value);
 
+    alumnos.push(alumno);
+    
+   document.getElementById("resultado").innerHTML = alumnos.join("<br/>");
+}
