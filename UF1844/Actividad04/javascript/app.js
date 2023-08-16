27f6c9b5d9f9fabea88add1168636928407a1cf9
Eleazar01 <eleazargon8@msn.com>
2023-08-16 20:01:39 +0100

    temas

--- UF1844/Actividad04/javascript/app.js
+++ UF1844/Actividad04/javascript/app.js
@@ -15,20 +15,22 @@ document.querySelector("#acceso").addEventListener("click", function acceso() {
     
     document.querySelector('.contenido').innerHTML = `
         <div>
-            <form action="https://reqres.in/api/users" id="formulario" method="post">
-            <input type="text" name="usuario" placeholder="Introduce el usuario"/>
-            <input type="password" name="password" placeholder="Introduce la contraseña"/>
-            <input type="submit" value="Validar">
+            <form id="formulario" method="post">
+            <input type="text" name="usuario id ='usuario'" placeholder="Introduce el usuario"/>
+            <input type="password" name="password" id = 'pasword' placeholder="Introduce la contraseña"/>
+            <button id="validar">Acceder</button>
             </form>
         </div>`
 });
 
 //Da el contenido del boton de registro
 
+// action="https://reqres.in/api/users"
+
 document.querySelector("#registro").addEventListener("click", function registro() {
     document.querySelector('.contenido').innerHTML = `
         <div>
-            <form action="https://reqres.in/api/users" id="formulario" method="post">
+            <form id="formulario" method="post">
             <input type="text" name="nombre" placeholder="Introduce el nombre"/>
             <input type="text" name="apellidos" placeholder="Introduce los apellidos"/>
             <input type="email" name="email" placeholder="Introduce el correo electronico"/>
@@ -36,7 +38,7 @@ document.querySelector("#registro").addEventListener("click", function registro(
             <input type="password" name="password2" placeholder="Repite la contraseña"/>
             <input type="number" name="edad" placeholder="Introduce la edad"/>
             <input type="url" name="fotografia" placeholder="Introduce una url de una fotografia"/>
-            <input type="submit" value="Registrar">
+            <button id="guardar">Registrar</button>
             </form>
         </div>`
 });
@@ -70,6 +72,31 @@ document.querySelector("#contacto").addEventListener("click", function contacto(
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat temporibus laboriosam quisquam consequuntur cupiditate veritatis molestias labore quibusdam animi neque, quas qui nesciunt! Laudantium necessitatibus recusandae voluptatibus ut aut maxime?</p>
         </div>`
 });
+
+var alumno = {
+    nombre : 'Eleazar', 
+    apellido: 'Gonzalez',
+    correo : 'eleazar@gmail.com',
+    edad : '15',
+    contraseña: '1234',
+    avatar:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.adslzone.net%2Fcomo-se-hace%2Fphotoshop%2Fquitar-fondo-imagen%2F&psig=AOvVaw0CSG97WRRv032NwvwHl7og&ust=1692295577376000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCIDS6M3h4YADFQAAAAAdAAAAABAE'
+  }
+
+  // validacion
+
+  document.querySelector('#validar').addEventListener('click',function () {
+    let validacion =['#usuario','#pasword'];
+
+    if (validacion[0] == alumno[0] && validacion[1] == alumno[4]) {
+        document.querySelector('#perfil')
+        .innerHTML(alumno[0]+alumno[1]);
+
+    } else {
+        document.querySelector('#perfil')
+        .innerHTML('Usuario o Contraseña incorrecta');
+    }
+
+  })
 
 
 
--- UF1844/Actividad04/javascript/jquery.js
+++ UF1844/Actividad04/javascript/jquery.js
@@ -1,10 +1,54 @@
 $(document)
 .ready(function () {
+
   //crea el acordeon del aside
   $( "#accordion" )
   .accordion({
     collapsible: true
   });
-  
 
+  //Tema principal
+
+  $('#color1')
+  .click(function () {
+    $(':root')
+    .css({'--fondo': '#484857e9','--detalle': '#c2d07c','--letra': '#c1bfbd','--cabeceras': '#03376e'})
+
+    $('*')
+    .css('font-size','1em');
+  });
+
+  //Tema azul
+
+  $('#color2')
+  .click(function () {
+    $(':root')
+    .css({'--fondo': '#090774','--detalle': '#403e48','--letra': '#a8a8a8','--cabeceras': '#755ed4'})
+
+    $('*')
+    .css('font-size','1em');
+  });
+
+  //Tema gris
+
+  $('#color3')
+  .click(function () {
+    $(':root')
+    .css({'--fondo': '#4a4a4a','--detalle': '#313131','--letra': '#cecece','--cabeceras': '#575252'})
+
+    $('*')
+    .css('font-size','1em');
+  });
+
+  //Tema altocontraste
+
+  $('#color4')
+  .click(function () {
+    $(':root')
+    .css({'--fondo': '#000000','--detalle': '#181515','--letra': '#ffffff','--cabeceras': '#181515'});
+
+    $('*')
+    .css('font-size','1.2em');
+  });
 });
+ 
