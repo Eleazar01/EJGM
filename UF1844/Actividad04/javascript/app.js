1ab1816aac63b3659e450c6bcfc1ef00db915f00
Eleazar01 <eleazargon8@msn.com>
2023-08-17 19:42:06 +0100

    a

--- UF1844/Actividad04/javascript/app.js
+++ UF1844/Actividad04/javascript/app.js
@@ -1,6 +1,11 @@
 
 //carga informacion de inicio
 
+var perfiles = [];
+var perfil = {};
+
+
+
 window.addEventListener('load', function informacion() {
     document.querySelector('.contenido').innerHTML = `
         <div>
@@ -16,19 +21,38 @@ document.querySelector("#acceso").addEventListener("click", function acceso() {
     document.querySelector('.contenido').innerHTML = `
         <div>
             <form id="formulario">
-            <input type="text" name="usuario id ='usuario'" placeholder="Introduce el usuario"/>
+            <input type="text" name="usuario" id ='usuario' placeholder="Introduce el usuario"/>
             <input type="password" name="password" id = 'password' placeholder="Introduce la contraseña"/>
             <button id="validar">Acceder</button>
             </form>
-        </div>`
+        </div>`;
 
-    // validar
+    // validación
 
-    document.querySelector('#validar').addEventListener('click', function () {
-        document.querySelector('#perfil').innerHTML = `<p>hola</p>`;
+    document.querySelector('#validar').addEventListener('click', function (e) {
+        e.preventDefault();
+
+        let nombre = "Manuel";
+        let password = 'admin';
+
+        let nombrev = document.querySelector('#usuario').value;
+        let passwordv = document.querySelector('#password').value;
+        if (nombrev == ''|| nombrev == null) {
+            document.querySelector('#perfil').innerHTML = `<p>Debes rellenar el campo`;
+        } else {
+            if (nombre == nombrev && password == passwordv) {
+                document.querySelector('#perfil').append(nombre);
+            } else {
+                document.querySelector('#perfil').innerHTML = `<p>Nombre de usuario o contraseña incorrecto</p>`;
+            } 
+        }
+        
+
     });
 
 });
+
+// document.querySelector('#perfil').innerHTML = `<p>Profesor</p><br><p>1º A</p>`; 
 
 //Da el contenido del boton de registro
 
@@ -38,16 +62,49 @@ document.querySelector("#registro").addEventListener("click", function registro(
     document.querySelector('.contenido').innerHTML = `
         <div>
             <form id="formulario">
-            <input type="text" name="nombre" placeholder="Introduce el nombre"/>
-            <input type="text" name="apellidos" placeholder="Introduce los apellidos"/>
-            <input type="email" name="email" placeholder="Introduce el correo electronico"/>
-            <input type="password" name="password" placeholder="Introduce una contraseña"/>
-            <input type="password" name="password2" placeholder="Repite la contraseña"/>
-            <input type="number" name="edad" placeholder="Introduce la edad"/>
-            <input type="url" name="fotografia" placeholder="Introduce una url de una fotografia"/>
-            <button id="guardar">Registrar</button>
+
+                <input type="text" name="nombre" id ='nombrer' placeholder="Introduce el nombre"/>
+
+                <input type="text" name="apellidos" id ='apellidor' placeholder="Introduce los apellidos"/>
+
+                <input type="email" name="email" id ='emailr' placeholder="Introduce el correo electronico"/>
+
+                <input type="password" name="password" id ='passwordr' placeholder="Introduce una contraseña"/>
+
+                <input type="password" name="password2" placeholder="Repite la contraseña"/>
+
+                <input type="number" name="edad" id ='edadr' placeholder="Introduce la edad"/>
+
+                <input type="url" name="fotografia" id ='avatarr' placeholder="Introduce una url de una fotografia"/>
+
+                <button id="guardar">Registrar</button>
+
             </form>
         </div>`
+
+        document.querySelector('#guardar').addEventListener('click', function (e) {
+            e.preventDefault();
+            perfil = {
+                nombre : document.querySelector('#nombrer').value,
+
+                apellidos : document.querySelector('#apellidor').value,
+
+                email :document.querySelector('#emailr').value,
+
+                contra :document.querySelector('#passwordr').value,
+
+                edad :document.querySelector('#edadr').value,
+
+                avatar : document.querySelector('#avatarr').value
+            }
+
+            perfiles.push(perfil);
+
+            localStorage.setItem('Perfiles', JSON.stringify(perfiles));
+
+        })
+
+
 });
 
 //Da el contenido del boton de informacion
