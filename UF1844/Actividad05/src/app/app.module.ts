33ccfba1ac273505213c57a7ca50d5c9ad7a691d
Eleazar01 <eleazargon8@msn.com>
2023-08-22 18:30:50 +0100

    1

--- UF1844/Actividad05/src/app/app.module.ts
+++ UF1844/Actividad05/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoComponent } from './curso/curso.component';
-import { AlumnosComponent } from './alumnos/alumnos.component';
-import { AlumnoComponent } from './alumno/alumno.component';
-import { ProfesoresComponent } from './profesores/profesores.component';
-import { ProfesorComponent } from './profesor/profesor.component';
+import { CursosComponent } from './componentes/cursos/cursos.component';
+import { CursoComponent } from './componentes/curso/curso.component';
+import { AlumnosComponent } from './componentes/alumnos/alumnos.component';
+import { AlumnoComponent } from './componentes/alumno/alumno.component';
+import { ProfesoresComponent } from './componentes/profesores/profesores.component';
+import { ProfesorComponent } from './componentes/profesor/profesor.component';
 import { AccesoComponent } from './componentes/acceso/acceso.component';
-import { RegistroComponent } from './registro/registro.component';
-import { ConsultaComponent } from './consulta/consulta.component';
-import { CabeceraComponent } from './cabecera/cabecera.component';
-import { FooterComponent } from './footer/footer.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ConsultaComponent } from './componentes/consulta/consulta.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
