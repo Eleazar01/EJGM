6048880596bbdcf27cb22cc78209a95d097309c9
Eleazar01 <eleazargon8@msn.com>
2023-08-23 19:24:00 +0100

    1

--- UF1844/Actividad05/src/app/componentes/registro/registro.component.ts
+++ UF1844/Actividad05/src/app/componentes/registro/registro.component.ts
@@ -12,17 +12,17 @@ export class RegistroComponent {
   public registroe : string
   public registrop1 : string
   public registrop2 : string
-  public registroed : number
+  public registroed : string
   public registroi : string
   
   constructor(){
-    this.imagen = '../../../assets/logo.jpeg'
-    this.registron = ''
-    this.registroa = ''
-    this.registroe = ''
-    this.registrop1 = ''
-    this.registrop2 = ''
-    this.registroed = ''
-    this.registroi = ''
+    this.imagen = '../../../assets/logo.jpeg';
+    this.registron = '';
+    this.registroa = '';
+    this.registroe = '';
+    this.registrop1 = '';
+    this.registrop2 = '';
+    this.registroed = '';
+    this.registroi = '';
   }
 }
