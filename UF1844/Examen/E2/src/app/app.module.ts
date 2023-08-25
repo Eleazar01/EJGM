affc12a9b4fa73838f08c60a848dcef65ad100b0
Eleazar01 <eleazargon8@msn.com>
2023-08-25 16:08:15 +0100

    e2

---
+++ UF1844/Examen/E2/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { InfoComponent } from './componentes/info/info.component';
+import { TiendaComponent } from './componentes/tienda/tienda.component';
+import { AccesoComponent } from './componentes/acceso/acceso.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { CarritoComponent } from './componentes/carrito/carrito.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabeceraComponent,
+    InfoComponent,
+    TiendaComponent,
+    AccesoComponent,
+    RegistroComponent,
+    FooterComponent,
+    CarritoComponent,
+    ContactoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
