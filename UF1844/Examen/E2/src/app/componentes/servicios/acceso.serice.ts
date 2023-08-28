7114093c7d00a174f33b52cc7a827036050e524b
Eleazar01 <eleazargon8@msn.com>
2023-08-28 17:08:33 +0100

    1

--- UF1844/Examen/E2/src/app/componentes/servicios/acceso.serice.ts
+++ UF1844/Examen/E2/src/app/componentes/servicios/acceso.serice.ts
@@ -1,14 +1,1 @@
-import { Injectable } from "@angular/core";
-import { RegistroComponent } from "../registro/registro.component";
 
-@Injectable({
-    providedIn: 'root'
-})
-export class RegistroService{
-    private accesoall: Array<RegistroComponent>
-    constructor(){
-        this.accesoall=[
-            new RegistroComponent()
-        ]
-    }
-}
