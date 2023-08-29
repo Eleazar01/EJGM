46037339cb92f687153a428f8e678c331e0b2a71
Eleazar01 <eleazargon8@msn.com>
2023-08-29 19:55:56 +0100

    1

--- UF1844/Actividad05/src/app/componentes/alumnos/alumnos.component.ts
+++ UF1844/Actividad05/src/app/componentes/alumnos/alumnos.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AlumnoBD } from 'src/app/models/alumno';
+import { CursosService } from 'src/app/Servicios/curso.service';
 
 @Component({
   selector: 'app-alumnos',
   templateUrl: './alumnos.component.html',
   styleUrls: ['../../app.component.css']
 })
-export class AlumnosComponent {
+export class AlumnosComponent implements OnInit{
+  public alumnosall = new Array<AlumnoBD>
   public imagen : string 
   public alumnosAll: Array<AlumnoBD>
   
-  constructor(){
+  constructor(private _cursosService: CursosService){
     this.imagen = '../../../assets/logo.jpeg'
     this.alumnosAll = [
       new AlumnoBD('Miguel','Ramírez', 39, 'miguel@gmail.com','555 555 555 ', '../../assets/avatar.png',true),
@@ -19,5 +21,9 @@ export class AlumnosComponent {
       new AlumnoBD('Cristo','Ramírez', 31, 'javier@gmail.com','555 555 555 ', '../../assets/avatar.png',false),
       new AlumnoBD('Kilian','Ramírez', 60, 'javier@gmail.com','555 555 555 ', '../../assets/avatar.png',false)
     ]
+  }
+   ngOnInit(): void {
+    // this.alumnosAll = this._cursosService.getAlumnos();
+    // this.getcorreosEle();
   }
 }
