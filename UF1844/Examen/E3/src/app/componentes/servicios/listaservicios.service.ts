fe1d5fdba400ed9ab198bf54013cee61780adb46
Eleazar01 <eleazargon8@msn.com>
2023-08-31 17:20:39 +0100

    1

--- UF1844/Examen/E3/src/app/componentes/servicios/listaservicios.service.ts
+++ UF1844/Examen/E3/src/app/componentes/servicios/listaservicios.service.ts
@@ -8,10 +8,10 @@ export class ListaserviciosService {
   private listaServicios: Array<ServiciosBD>
   constructor() { 
     this.listaServicios = [
-                new ServiciosBD("100", "Consultoría","Servicio de consultoría sobre formación en programación frontend, backend y fullstack", 60,"./assets/media/consultoria.png", true), 
-                new ServiciosBD("101", "Formación Frontend","Servicio de formación sobre  programación frontend", 160,"./assets/media/frontend.jpg", true), 
-                new ServiciosBD("102", "Formación Backend","Servicio de formación sobre  programación Backend", 160,"./assets/media/backend.png", true),
-                new ServiciosBD("103", "Formación FullStack","Servicio de formación sobre  programación FullStock", 300,"./assets/media/fullstack.jpg", true)
+                new ServiciosBD("100", "Sastrería","Servicio de sastrería por prenda", 20,"../../../assets/sastre.jpg", true), 
+                new ServiciosBD("101", "Estampado de ropa","Servicio de estampado en ropa con plantilla de cliente", 30,"../../../assets/estampado2.jpeg", true), 
+                new ServiciosBD("102", "Estampado de ropa","Servicio de estamapdo en ropa, realizando nosotros el diseño", 60,"../../../assets/estampado.png", true),
+                new ServiciosBD("103", "Lavandería","Servicio de lavandería", 20,"../../../assets/lavanderia.jpg", true)
               ]
   }
   getServicios():Array<ServiciosBD>{
