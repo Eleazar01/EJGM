3f41a9d9758b2f8f5d6c6113580e111e19f81352
Eleazar01 <eleazargon8@msn.com>
2023-09-05 17:03:41 +0100

    1

---
+++ UF1844/Actividad05/servernode/config/dbase.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const DB_URI ='mongodb://localhost:27017/Appweb'
+
+module.exports = ()=> {
+    const connect = () =>{
+        mongoose.connect(DB_URI,
+            {
+                keepAlive: true,
+                useNewUrlParse: true,
+                useUnifieldTopology: true
+            }
+        ).then(()=> {
+            console.log('Conexión Directa')
+        })
+        .catch((err)=>{
+            console.log('DB: ERROR!!')
+        })
+    }
+
+    connect()
+
+}
