4fb9e7265952ebe97fccea8071e94660675dcefa
Eleazar01 <eleazargon8@msn.com>
2023-09-06 18:56:03 +0100

    1

--- UF1844/Actividad05/servernode/controllers/cursos.js
+++ UF1844/Actividad05/servernode/controllers/cursos.js
@@ -1,1 +1,95 @@
-res.send({data: 'Esto viene de Rutas'})
+const model = require('../models/cursos')
+
+const fs = require('fs');
+const path = require('path');
+const cursos = require('../models/cursos');
+
+const controller = {
+    home: function (req,res) {
+        return res.status(200).send({
+            message: 'Soy home de cursos'
+        });
+    },
+    test: function (req,res) {
+        return res.status(200).send({
+            message: 'Soy el metodo o acción test del controlador de cursos'
+        });
+    },
+    saveCursos: function (req,res) {
+        const cursos = new Model();
+        const params = req.body;
+        cursos.name = params.name;
+        cursos.description = params.description;
+        cursos.category = params.category;
+        cursos.year = params.year;
+        cursos.lang = params.langs;+
+        cursos.save((err,cursosStored)=>{
+            if(err) return res.status(500).send({message: 'Error al guardar el documento.'});
+            if(!projectStored) return res.status(404).send({message: 'No se ha podido guardar.'});
+            return res.status(200).send({cursos: cursosStored});
+        });
+    },
+    getCurso: function (req,res) {
+        var cursosId = req.params.id;
+
+        if(cursosId == null) return res.status(404).send({message: 'El curso no existe.'});
+
+        User.findById(cursosId, (err, cursos)=>{
+            if (err) return res.status(500).send({message: 'Error al devolver los datos.'});
+
+            if(!cursos) return res.status(404).send({message: 'El curso no existe.'});
+
+            return res.status(200).send({
+                cursos
+            });
+        });   
+    },
+
+    getCursos: function (req,res) {
+        User.find({}).sort('-year').exec((err, cursos)=>{
+            if(err) return res.status(500).send({message: 'Error al devolver los datos'});
+            if(!projects) return res.status(404).send({message: 'No hay usuarios ue mostrar'});
+            return res.status(200).send({projects});
+        });
+    },
+
+    updateCursos: function (req,res) {
+        var cursosId = req.params.id;
+        var update = req.body;
+        cursos.findByIdAndUpdate(cursosId, update, {new: true}, (err, cursosUpdate)=>{
+            if (err) return res.status(500).send({message: 'Error al actualizar'});
+            if (!cursosUpdate) return res.status(404).send({message: 'No existe el curso para actualizar'});
+            return res.status(200).send({
+                cursos: cursosUpdate
+            });
+        });
+    },
+
+    deleteCursos: function (req, res) {
+
+        var cursosId = req.params.id;
+
+        User.findByIdAndRemove(cursosId, (err, cursosRemoved)=>{
+            if (err) return res.status(500).send({message: 'No se ha podido borrar el curso'});
+            if (!cursosRemoved) return res.status(404).send({message: 'No se puede eliminar ese curso'});
+            return res.status(200).send({
+                cursos: cursosRemoved
+            });
+        });
+    },
+
+};
+
+module.exports = controller;
+
+
+
+// res.send({data: 'Esto viene de Rutas'})
+
+// exports.getData = (req,res)=>{
+//     model.find({}, (err, docs)=>{
+//         res.send({
+//         docs: docs
+//         })
+//     }) 
+// }
--- UF1844/Actividad05/servernode/routes/cursos.js
+++ UF1844/Actividad05/servernode/routes/cursos.js
@@ -1,12 +1,18 @@
 const express = require ('express')
 
-const controller = require('../controllers/cursos')
+const CursosController = require('../controllers/cursos')
 const router = express.Router()
 
-const path = 'cursos'
+router.get('/homecursos', CursosController.home);
+router.get('/testcursos', CursosController.test);
+router.post('/save-cursos', CursosController.saveCursos);
+router.get('/cursos/:id?', CursosController.getCurso);
+router.get('/cursos', CursosController.getCursos);
+router.put('/cursos/:id', CursosController.updateCursos);
+router.delete('/cursos/:id', CursosController.deleteCursos);
 
-router.get(
-    `/${path}`,
-    controller.getData
-)
+// const path = 'cursos'
+//     `/${path}`,
+//     CursosController.getData
+// )
 module.exports = router
