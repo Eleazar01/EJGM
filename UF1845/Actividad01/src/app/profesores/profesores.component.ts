5ec8c705e10ee3a025e0d418524c2d1524fd5452
Eleazar01 <eleazargon8@msn.com>
2023-09-13 18:43:56 +0100

    1

--- UF1845/Actividad01/src/app/profesores/profesores.component.ts
+++ UF1845/Actividad01/src/app/profesores/profesores.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-profesores',
+  exportAs: 'cdkListboxReactiveFormsExample',
   templateUrl: './profesores.component.html',
   styleUrls: ['./profesores.component.css']
 })
 export class ProfesoresComponent {
-
+  languages = ['Jose', 'Manuel', 'Elena', 'Daoli', 'Ana'];
+  languageCtrl = new FormControl(['Manuel']);
 }
