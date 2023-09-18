b674cf2badef9acf0d804f503b923c8bb4f43c94
Eleazar01 <eleazargon8@msn.com>
2023-09-18 15:23:34 +0100

    1

--- UF1845/Actividad01/nodeserve/app/server.js
+++ UF1845/Actividad01/nodeserve/app/server.js
@@ -6,28 +6,6 @@ const cors = require("cors")
 // Creamos el servidor
 const app = express()
 
-// const port = 3700
-// app.get('/', (req, res) =>{
-//     res.send('Servidor en linea por pantalla...')
-// })
-// app.listen(port,() =>{
-//     console.log('La aplicación está en línea....')
-// })
-// const cursoRouters = require('./app/routes/curso')
-// app.use(cursoRouters)
-
-// Conexión con Base datos
-// mongoose.Promise = global.Promise
-// mongoose.connect('mongodb://127.0.0.1:27017/Appweb', {
-//     useNewUrlParser: true,
-// })
-// .then(() => {
-//     console.log("Conexión establecida...")
-// })
-// .catch(err => console.log(err))
-
-
-// Conectamos DB Cursos
 conectarDB();
 app.use(cors())
 
