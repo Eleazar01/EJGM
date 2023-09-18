2e050b2954ae14e2bda96518ba8a5eee2344a73b
Eleazar01 <eleazargon8@msn.com>
2023-09-18 20:01:49 +0100

    1

--- UF1845/Examen/E2/servernode/routes/pevalua.js
+++ UF1845/Examen/E2/servernode/routes/pevalua.js
@@ -5,10 +5,10 @@ const router = express.Router()
 
 router.get('/home', PevaluaController.home);
 router.post('/test', PevaluaController.test);
-router.post('/save-user', PevaluaController.saveEvalua);
-router.get('/user/:id?', PevaluaController.getEvalua);
-router.get('/users', PevaluaController.getEvalua);
-router.put('/user/:id', PevaluaController.updateEvalua);
-router.delete('/user/:id', PevaluaController.deleteEvalua);
+router.post('/save-pevalua', PevaluaController.saveEvalua);
+router.get('/pevalua/:id?', PevaluaController.getEvalua);
+router.get('/pevalua', PevaluaController.getEvalua);
+router.put('/pevalua/:id', PevaluaController.updateEvalua);
+router.delete('/pevalua/:id', PevaluaController.deleteEvalua);
 
 module.exports = router
