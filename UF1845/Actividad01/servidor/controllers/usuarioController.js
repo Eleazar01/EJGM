7dc48c6eb99909e95cd9c40b6489fa8c5904817a
Eleazar01 <eleazargon8@msn.com>
2023-09-19 20:05:29 +0100

    1

---
+++ UF1845/Actividad01/servidor/controllers/usuarioController.js
@@ -0,0 +1,78 @@
+const Usuario = require('../models/usuarioModel');
+
+exports.crearUsuarios = async (req, res) => {
+    
+    try {
+        let usuario = new Usuario(req.body)
+        await usuario.save()
+        res.send(usuario)
+    }catch(error){
+        console.log(error)
+        res.status(500).send('Hubo un error')
+    }
+}
+
+exports.obtenerUsuarios = async (req, res) => {
+    
+    try {
+        const usuarios = await Usuario.find()
+        res.json(usuarios)
+    }catch(error){
+        console.log(error)
+        res.status(500).send('Hubo un error')
+    }
+}
+    
+exports.obtenerUsuario = async (req, res) => {
+    
+    try {
+        let usuario = await Usuario.findById(req.params.id)
+        if(!usuario){
+            res.status(404).json({msg:'El usuario no existe'})
+        }
+        res.json(usuario)
+    }catch(error){
+        console.log(error)
+        res.status(500).send('Hubo un error')
+    }
+}
+    
+exports.actualizarUsuario = async (req, res) => {
+    
+    try {
+        const {nombre, apellidos, Fnacimiento, email, telefono, foto, pass, tipo} = req.body
+        let usuario = await Usuario.findById(req.params.id)
+        if(!usuario){
+            res.status(404).json({msg:'Usuario no encontrado'})
+        }
+        usuario.nombre = nombre
+        usuario.apellidos = apellidos
+        usuario.Fnacimiento = Fnacimiento
+        usuario.email = email
+        usuario.telefono = telefono
+        usuario.foto = foto
+        usuario.pass = pass
+        usuario.tipo = tipo
+        usuario = await Usuario.findOneAndUpdate({_id: req.params.id}, usuario, {new: true})
+        res.json(usuario)
+
+    }catch(error){
+        console.log(error)
+        res.status(500).send('Hubo un error')
+    }
+}
+    
+exports.eliminarUsuario = async (req, res) => {
+    
+    try {
+        let usuario = await Usuario.findById(req.params.id)
+        if(!usuario){
+            res.status(404).json({msg:'El usuario no existe'})
+        }
+        await Usuario.findOneAndRemove({_id: req.params.id})
+        res.json({msg:'Usuario eliminado'})
+    }catch(error){
+        console.log(error)
+        res.status(500).send('Hubo un error')
+    }
+}
---
+++ UF1845/Actividad01/servidor/models/usuarioModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UsuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    apellidos: {
+        type: String,
+        required: true
+    },
+    Fnacimiento: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    telefono: {
+        type: String,
+        required: true
+    },
+    foto: {
+        type: String,
+        required: true
+    },
+    pass: {
+        type: String,
+        required: true
+    },
+    tipo: {
+        type: String,
+        required: true
+    },
+    fechaCreacion: {
+        type: Date,
+        default: Date.now()
+    }
+    
+});
+
+//Exportamos el modelo  
+
+module.exports = mongoose.model('Usuario', UsuarioSchema);
