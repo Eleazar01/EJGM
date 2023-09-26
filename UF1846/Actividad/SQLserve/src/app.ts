4046cbd6eaaf1a14b43dee081d67007d828a98a1
Eleazar01 <eleazargon8@msn.com>
2023-09-26 19:32:48 +0100

    1

--- UF1846/Actividad/SQLserve/src/app.ts
+++ UF1846/Actividad/SQLserve/src/app.ts
@@ -3,7 +3,7 @@ import express  from 'express';
 import cors from 'cors';
 // import { router } from './routes';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors());
