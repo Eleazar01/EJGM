18a9ac47c96e9af982ddd41c0e54010532d95952
Eleazar01 <eleazargon8@msn.com>
2023-09-29 18:48:53 +0100

    1

--- UF1846/Actividad/sqlserver/src/models/MensajeModel.ts
+++ UF1846/Actividad/sqlserver/src/models/MensajeModel.ts
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
 import db from '../config/connectdb';
 
-const Mensaje = db.define('mensajes',{
+const Mensaje = db.define('mensaje',{
     remite: {
         type: DataTypes.STRING
     },
